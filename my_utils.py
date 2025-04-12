8386a0007956066dfcb98b55892d62aecbf6d62b
Ryan Garaygay <ryan.garaygay@gmail.com>
2025-04-11 22:09:36 -0700

    removed unused function in my_utils

--- my_utils.py
+++ my_utils.py
@@ -1,30 +1,6 @@
 from datetime import timedelta, datetime
 from constants import CONST
 
-def find_key_with_lowest_order_id(data):
-    """
-    Finds the key in a defaultdict(list) that contains the Trade object with the
-    lowest order_id among all Trade objects in all lists.
-
-    Args:
-        data: The defaultdict(list) where values are lists of Trade objects.
-
-    Returns:
-        The key with the lowest order_id, or None if the defaultdict is empty.
-    """
-    lowest_order_id = float('inf')  # Initialize with positive infinity
-    key_with_lowest = None
-
-    for key, trades in data.items():
-        for trade in trades:
-            number_only_order_id = int(re.sub(r"[^0-9]", "", trade.order_id))
-            if number_only_order_id < lowest_order_id:
-            # if trade.order_id < lowest_order_id:
-                lowest_order_id = number_only_order_id
-                key_with_lowest = key
-
-    return key_with_lowest
-
 def filter_namedtuples(list_of_namedtuples, attribute_name, target_value):
     """
     Filters a list of namedtuples based on the value of a specific attribute.
